ae6cc894a9abd2125fc1caa534eb387f626bac34
Gurleen55 <gurleencan6@gmail.com>
2025-04-02 12:18:49 -0300

    configure factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe User, type: :model do
   it { should have_many(:passive_relationships).class_name("Relationship").with_foreign_key("followed_id").dependent(:destroy) }
   it { should have_many(:following).through(:active_relationships).source(:followed) }
   it { should have_many(:followers).through(:passive_relationships).source(:follower) }
+
+  describe "#follow" do
+    it "follows the other user" do
+    end
+  end
 end
